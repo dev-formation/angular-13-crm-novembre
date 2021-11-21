6083960a67961762c5b96d3721ecaff253a4162d
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-11-21 11:40:54 +0100

    2 - import coreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiModule } from '../ui/ui.module';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { LoginModule } from '../login/login.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    UiModule,
+    IconsModule,
+    TemplatesModule,
+    LoginModule
   ]
 })
 export class CoreModule { }
