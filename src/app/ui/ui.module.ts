09deff41778848c94f6af410d9f51748a857ed0f
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-11-21 15:46:55 +0100

    7.1 - Correction TP + demo UI2

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { UiComponent } from './components/ui/ui.component';
 import { HeaderComponent } from '../core/components/header/header.component';
 import { CoreModule } from '../core/core.module';
+import { Ui2Component } from './components/ui2/ui2.component';
 
 
 
 @NgModule({
   declarations: [
-    UiComponent
+    UiComponent,
+    Ui2Component
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    UiComponent
+    UiComponent,
+    Ui2Component
   ]
 })
 export class UiModule { }
