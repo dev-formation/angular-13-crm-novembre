f9438274a35c849dd05cc6ea7f27625f3e9007d7
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2021-11-24 22:18:36 +0100

    34 - validators + css

--- src/app/orders/components/form-order/form-order.component.ts
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 
@@ -18,15 +18,17 @@ export class FormOrderComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      tjmHt: [this.init.tjmHt],
+      tjmHt: [this.init.tjmHt, Validators.required],
       nbJours: [this.init.nbJours],
       tva: [this.init.tva],
       state: [this.init.state],
-      typePresta: [this.init.typePresta],
-      client: [this.init.client],
+      typePresta: [this.init.typePresta, Validators.required],
+      client: [this.init.client, [Validators.required,Validators.minLength(2)]],
       comment: [this.init.comment],
       id: [this.init.id]
     })
+    console.log(this.form.value);
+    
   }
 
   public onSubmit(): void {
